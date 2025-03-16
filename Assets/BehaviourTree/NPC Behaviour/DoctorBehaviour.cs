84076dfb7a81044a8d7cf31b8985b230f529ff32
ivos <35622622+larardossa@users.noreply.github.com>
2025-03-16 19:01:39 +0100

    patient behaviour

--- Assets/BehaviourTree/NPC Behaviour/DoctorBehaviour.cs
+++ Assets/BehaviourTree/NPC Behaviour/DoctorBehaviour.cs
@@ -1,38 +1,25 @@
+using System;
 using UnityEngine;
 
 public class DoctorBehaviour : BT_agent
 {
+    //private List<GameObject> patients = new List<GameObject>();
+    //public GameObject[] patients;
     
-    public GameObject[] patients;
-    
-    new void Start()
-    {
-        base.Start();
-    
-        RSelector selectObject = new RSelector("Select Object");
-        for (int i = 0; i < patients.Length; i++)
-        {
-            Leaf gtt = new Leaf("Go to " + patients[i].name, i, GoToTech);
-            selectObject.AddChild(gtt);
-        }
-        
-        tree.AddChild(selectObject);
-        //tree.PrintTree();
-    }
-    
-    
-    public Node.Status GoToTech(int i)
-    {
-        if(!patients[i].activeSelf)
-        {
-            return Node.Status.FAILURE;
-        }
-        Node.Status s = GoToLocation(patients[i].transform.position);
-        if (s == Node.Status.SUCCESS)
-        {
-            patients[i].SetActive(false);
-        }
-        return s;
-    }
-    
+    // new void Start()
+    // {
+    //     base.Start();
+    //     
+    // }
+    //
+    //
+    // private void Update()
+    // {
+    //     UpdatePatientsList();
+    // }
+    //
+    // private void UpdatePatientsList()
+    // {
+    //     patients = PatientManager.Instance.GetPatients();
+    // }
 }
--- Assets/BehaviourTree/NPC Behaviour/PatientBehaviour.cs
+++ Assets/BehaviourTree/NPC Behaviour/PatientBehaviour.cs
@@ -1,32 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PatientBehaviour : BTAgent
 {
     public GameObject register;
     public GameObject[] rooms;
+    public Transform hospitalExit;
     new void Start()
     {
         base.Start();
         register = GameObject.Find("Register");
         rooms = GameObject.FindGameObjectsWithTag("Room");
+        hospitalExit = GameObject.Find("HospitalExit").transform;
         
-        Sequence goToRegister = new Sequence("Register Sequence");
-        Leaf goToRegisterDesk = new Leaf("Go to Register Desk", GoToRegisterDesk);
-        goToRegister.AddChild(goToRegisterDesk);
+        Sequence RootSeq = new Sequence("Root Sequence");
         
-        RSelector goToRandomRoom = new RSelector("RandomRoom");
+        //Sequence for patient to go to register
+        Sequence registerSequence = new Sequence("Register Sequence");
+        Leaf goingToRegister = new Leaf("Go to Register", GoToRegisterDesk);
+        Leaf waitingForDiagnostic = new Leaf("Wait for Diagnostic", WaitForDiagnostic);
+        registerSequence.AddChild(goingToRegister);
+        registerSequence.AddChild(waitingForDiagnostic);
         
+        //Sequence for patient to go to room
+        Sequence gettingTreated = new Sequence("Getting Treated");
+        RSelector randomRoomSelector = new RSelector("Random Room Selector");
         for (int i = 0; i < rooms.Length; i++)
         {
             Leaf goToRoom = new Leaf("Go to Room " + i, i, GoToRoom);
-            goToRandomRoom.AddChild(goToRoom);
+            randomRoomSelector.AddChild(goToRoom);
         }
-        goToRegister.AddChild(goToRandomRoom);
+        gettingTreated.AddChild(randomRoomSelector);
+        Leaf goToExit = new Leaf("Go to Exit", GoToHospitalExit);
+        gettingTreated.AddChild(goToExit);
         
-        tree.AddChild(goToRegister);
+        RootSeq.AddChild(registerSequence);
+        RootSeq.AddChild(gettingTreated);
+        tree.AddChild(RootSeq);
         tree.PrintTree();
+    }
+
+
+    private Node.Status WaitForDiagnostic()
+    {
+        Debug.Log("Wait for Diagnostic");
+        return Node.Status.SUCCESS;
     }
 
     private Node.Status GoToRoom(int val)
@@ -38,7 +58,16 @@ public class PatientBehaviour : BTAgent
         Node.Status s = GoToLocation(rooms[val].transform.position);
         if (s == Node.Status.SUCCESS)
         {
-            Debug.Log("reached room");
+            Debug.Log("Go to Room " + val);
+        }
+        return s;
+    }
+    private Node.Status GoToHospitalExit()
+    {
+        Node.Status s = GoToLocation(hospitalExit.transform.position);
+        if (s == Node.Status.SUCCESS)
+        {
+            Destroy(this.gameObject);
         }
         return s;
     }
@@ -46,6 +75,12 @@ public class PatientBehaviour : BTAgent
     private Node.Status GoToRegisterDesk()
     {
         Node.Status s = GoToLocation(register.transform.position);
+        if (s == Node.Status.SUCCESS)
+        {
+            Debug.Log("at register");
+        }
+        
         return s;
     }
+    
 }
