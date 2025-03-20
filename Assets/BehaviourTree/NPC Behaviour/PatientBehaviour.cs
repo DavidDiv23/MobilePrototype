b062fcb1dab02937ee8dc1d0caee6e0d47afcd8c
ivos <35622622+larardossa@users.noreply.github.com>
2025-03-20 21:37:24 +0100

    Fixed coroutines

--- Assets/BehaviourTree/NPC Behaviour/PatientBehaviour.cs
+++ Assets/BehaviourTree/NPC Behaviour/PatientBehaviour.cs
@@ -1,30 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public class PatientBehaviour : BTAgent
+public class PatientBehaviour : BT_agent
 {
     public GameObject register;
     public GameObject[] rooms;
     public Transform hospitalExit;
+    public int timeToWait = 10;
+    public bool isWaiting;
+
     new void Start()
     {
         base.Start();
         register = GameObject.Find("Register");
         rooms = GameObject.FindGameObjectsWithTag("Room");
         hospitalExit = GameObject.Find("HospitalExit").transform;
-        
+
         Sequence RootSeq = new Sequence("Root Sequence");
-        
-        //Sequence for patient to go to register
+
+        // Sequence for patient to go to register
         Sequence registerSequence = new Sequence("Register Sequence");
         Leaf goingToRegister = new Leaf("Go to Register", GoToRegisterDesk);
-        Leaf waitingForDiagnostic = new Leaf("Wait for Diagnostic", WaitForDiagnostic);
+        Leaf waitingForDiagnostic = new Leaf("Wait for Diagnostic", WaitAtRegister);
         registerSequence.AddChild(goingToRegister);
         registerSequence.AddChild(waitingForDiagnostic);
-        
-        //Sequence for patient to go to room
+
+        // Sequence for patient to go to room
         Sequence gettingTreated = new Sequence("Getting Treated");
         RSelector randomRoomSelector = new RSelector("Random Room Selector");
         for (int i = 0; i < rooms.Length; i++)
@@ -32,54 +34,76 @@ public class PatientBehaviour : BTAgent
             Leaf goToRoom = new Leaf("Go to Room " + i, i, GoToRoom);
             randomRoomSelector.AddChild(goToRoom);
         }
-        gettingTreated.AddChild(randomRoomSelector);
+
+        Leaf waitingForDoctor = new Leaf("Wait for Treatment", WaitForDoctor);
         Leaf goToExit = new Leaf("Go to Exit", GoToHospitalExit);
+
+        gettingTreated.AddChild(randomRoomSelector);
+        gettingTreated.AddChild(waitingForDoctor);
         gettingTreated.AddChild(goToExit);
-        
+
         RootSeq.AddChild(registerSequence);
         RootSeq.AddChild(gettingTreated);
         tree.AddChild(RootSeq);
         tree.PrintTree();
     }
 
-
-    private Node.Status WaitForDiagnostic()
+    private Node.Status GoToRegisterDesk()
     {
-        Debug.Log("Wait for Diagnostic");
+        Node.Status s = GoToLocation(register.transform.position);
+        isWaiting = true;
+        return s;
+    }
+
+    private Node.Status WaitAtRegister()
+    {
+        if (isWaiting)
+        {
+            StartCoroutine(WaitCoroutine(timeToWait)); // Start coroutine only once
+            return Node.Status.FAILURE;
+        }
         return Node.Status.SUCCESS;
     }
 
     private Node.Status GoToRoom(int val)
     {
-        if (val > rooms.Length)
+        if (val >= rooms.Length)
         {
             return Node.Status.FAILURE;
         }
+
         Node.Status s = GoToLocation(rooms[val].transform.position);
         if (s == Node.Status.SUCCESS)
         {
-            Debug.Log("Go to Room " + val);
+            Debug.Log("Arrived at Room " + val);
         }
         return s;
     }
+
+    private Node.Status WaitForDoctor()
+    {
+        if (!isWaiting)
+        {
+            StartCoroutine(WaitCoroutine(timeToWait));
+            return Node.Status.RUNNING;
+        }
+        return isWaiting ? Node.Status.RUNNING : Node.Status.SUCCESS;
+    }
+
     private Node.Status GoToHospitalExit()
     {
         Node.Status s = GoToLocation(hospitalExit.transform.position);
         if (s == Node.Status.SUCCESS)
         {
+            Debug.Log("Patient leaving hospital.");
             Destroy(this.gameObject);
         }
         return s;
     }
 
-    private Node.Status GoToRegisterDesk()
+    private IEnumerator WaitCoroutine(int waitTime)
     {
-        Node.Status s = GoToLocation(register.transform.position);
-        if (s == Node.Status.SUCCESS)
-        {
-            Debug.Log("at register");
-        }
-        
-        return s;
+        yield return new WaitForSeconds(waitTime);
+        isWaiting = false; 
     }
 }
