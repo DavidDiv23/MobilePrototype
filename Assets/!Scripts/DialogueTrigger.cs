5d5e9ee1fbfe66bcb49184c859246ca7c4dd2234
ivos <35622622+larardossa@users.noreply.github.com>
2025-04-03 15:51:26 +0200

    minor changes

--- Assets/!Scripts/DialogueTrigger.cs
+++ Assets/!Scripts/DialogueTrigger.cs
@@ -1,16 +1,42 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using NodeCanvas.Framework;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
 using Yarn.Unity;
 
 public class DialogueTrigger : MonoBehaviour, IPointerClickHandler
 {
-    public DialogueRunner dialogueRunner;
+    [SerializeField] private DialogueRunner _dialogueRunner;
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private BBParameter<bool> _isDialogueActive; // NodeCanvas Blackboard var
+
+    private void Start()
+    {
+        _dialogueRunner.onDialogueStart.AddListener(OnDialogueStart);
+        _dialogueRunner.onDialogueComplete.AddListener(OnDialogueEnd);
+    }
+
+    private void OnDialogueStart()
+    {
+        _isDialogueActive.value = true;
+        _agent.isStopped = true; // Stop moving
+    }
+
+    private void OnDialogueEnd()
+    {
+        _isDialogueActive.value = false;
+        _agent.isStopped = false; // Resume wandering
+    }
+
+    private void OnDestroy()
+    {
+        // Clean up listeners
+        _dialogueRunner.onDialogueStart.RemoveListener(OnDialogueStart);
+        _dialogueRunner.onDialogueComplete.RemoveListener(OnDialogueEnd);
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
-        dialogueRunner.StartDialogue("LionDialogue");
+        _dialogueRunner.StartDialogue("LionDialogue");
+        _dialogueRunner.onDialogueStart.AddListener(OnDialogueStart);
     }
 }
---
+++ Assets/!Scripts/SaveSystem/Serialization/SerializationManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SerializationManager
+{
+    // public static bool Save(string saveName, object saveData)
+    // {
+    //     BinaryFormatter formatter = GetBinaryFormatter();
+    //
+    //     if (!Directory.Exists(Application.persistentDataPath + "/Saves"))
+    //     {
+    //         Directory.CreateDirectory(Application.persistentDataPath + "/Saves");
+    //     }
+    //     
+    //     string path = Application.persistentDataPath + "/Saves/" + saveName + ".save";
+    //     
+    // }
+    //
+    // public static BinaryFormatter GetBinaryFormatter()
+    // {
+    //     BinaryFormatter formatter = new BinaryFormatter();
+    //     return formatter;
+    // }
+}
