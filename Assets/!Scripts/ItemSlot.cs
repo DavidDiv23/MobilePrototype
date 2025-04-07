0858ee13f3eb77715d1add9049a5944173fe377d
casperxp <casperxp2004@hotmail.com>
2025-04-07 21:43:54 +0200

    Shader push (test, might break)
    This reverts commit 64cadd4b1e0ab62e915f93d26001cb17c0490196.

--- Assets/!Scripts/ItemSlot.cs
+++ Assets/!Scripts/ItemSlot.cs
@@ -5,14 +5,12 @@ using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour, IDropHandler
 {
-    public static int counter;
     public void OnDrop(PointerEventData eventData)
     {
         if (eventData.pointerDrag != null)
         {
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-            counter++;
-            Debug.Log(counter);
-        }       
+        }
+            
     }
 }
