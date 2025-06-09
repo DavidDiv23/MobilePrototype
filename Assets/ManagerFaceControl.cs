daf21198be4c02f8c49129bfac4d5dfc5c8e7207
ivos <35622622+larardossa@users.noreply.github.com>
2025-06-09 17:31:07 +0200

    manager emotions in

--- Assets/ManagerFaceControl.cs
+++ Assets/ManagerFaceControl.cs
@@ -1,24 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class ManagerFaceControl : MonoBehaviour
 {
     public Material material;
+    public DialogueRunner dialogueRunner;
 
     [Range(0, 3)]
     public int face;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        if (dialogueRunner != null)
+        {
+            dialogueRunner.AddCommandHandler<string>("set_manager_face", HandleSetFace);
+        }
+        else
+        {
+            Debug.LogError("DialogueRunner reference is missing in ManagerFaceControl.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Vector2 offset = new Vector2(0f, -0.25f * face);
         material.SetTextureOffset("_BaseMap", offset);
+    }
+
+    public void SetFace(int faceIndex)
+    {
+        face = faceIndex;
+    }
+
+    private void HandleSetFace(string faceIndexStr)
+    {
+        if (int.TryParse(faceIndexStr, out int faceIndex))
+        {
+            var managerFace = FindObjectOfType<ManagerFaceControl>();
+            if (managerFace != null)
+            {
+                managerFace.SetFace(faceIndex);
+            }
+        }
+        else
+        {
+            Debug.LogError($"Invalid index passed to set_manager_face: {faceIndexStr}");
+        }
     }
 }
